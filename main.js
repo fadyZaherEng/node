999be8310796282468190aaa1fbebf19a003e138
Fady Zaher <fady33zaher@gmail.com>
2024-03-23 17:41:45 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//build in modules
+const osArch = require('node:os');
+const fs = require('node:fs');
+const path = require('node:path');
+console.log(osArch.arch());
+console.log(fs.ReadStream);
+console.log(path.dirname.name.toUpperCase());
+//user defined modules
+const calc = require('./calculator.js');
+console.log(calc.add(1, 2));
+console.log(calc.sub(2, 1));
+console.log(calc.mul(2, 3));
+console.log(calc.div(6, 2));
